c10b844c08806ae6f38a0a4f92cda614157e5795
unknown <smarquez@servicios.mc4.com.bo>
2024-10-30 23:16:15 -0400

    Add Lombok

--- src/main/java/com/prograMovil/demo/dtos/ConvocatoriaDTO.java
+++ src/main/java/com/prograMovil/demo/dtos/ConvocatoriaDTO.java
@@ -1,6 +1,15 @@
 package com.prograMovil.demo.dtos;
 
+import lombok.*;
+
 import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class ConvocatoriaDTO {
     private String titulo;
@@ -10,71 +19,4 @@ public class ConvocatoriaDTO {
     private Date fechaInicio;
     private Date fechaFin;
     private EmpresaDTO empresa;
-    public ConvocatoriaDTO() {}
-
-    public ConvocatoriaDTO(String titulo, String descripcion, String imagen, Integer cantidadMaxPost, Date fechaInicio, Date fechaFin, EmpresaDTO empresa) {
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.imagen = imagen;
-        this.cantidadMaxPost = cantidadMaxPost;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.empresa = empresa;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public Integer getCantidadMaxPost() {
-        return cantidadMaxPost;
-    }
-
-    public void setCantidadMaxPost(Integer cantidadMaxPost) {
-        this.cantidadMaxPost = cantidadMaxPost;
-    }
-
-    public Date getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(Date fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public Date getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(Date fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public EmpresaDTO getEmpresa() {
-        return empresa;
-    }
-
-    public void setEmpresa(EmpresaDTO empresa) {
-        this.empresa = empresa;
-    }
 }
--- src/main/java/com/prograMovil/demo/dtos/EmpresaDTO.java
+++ src/main/java/com/prograMovil/demo/dtos/EmpresaDTO.java
@@ -1,5 +1,13 @@
 package com.prograMovil.demo.dtos;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmpresaDTO {
     private Integer id;
     private String nombre;
@@ -9,82 +17,4 @@ public class EmpresaDTO {
     private String usuario;
     private String contrasenia;
     private String rol;
-
-    public EmpresaDTO() {
-    }
-
-    public EmpresaDTO(Integer id, String nombre, String ubicacion, String imagen, String nit, String usuario, String contrasenia, String rol) {
-        this.id = id;
-        this.nombre = nombre;
-        this.ubicacion = ubicacion;
-        this.imagen = imagen;
-        this.nit = nit;
-        this.usuario = usuario;
-        this.contrasenia = contrasenia;
-        this.rol = rol;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public String getNit() {
-        return nit;
-    }
-
-    public void setNit(String nit) {
-        this.nit = nit;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getContrasenia() {
-        return contrasenia;
-    }
-
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
 }
